33381ac57fe35d8bfe522811a2b428f27a788cc2
Pascal Horton <pascal.horton@giub.unibe.ch>
2022-10-26 09:18:59 +0200

    Mocking dependencies.

--- conf.py
+++ conf.py
@@ -23,6 +23,20 @@ sys.path.insert(0, os.path.join(BASE_PATH, "_deps", "hydrobricks", "python", "sr
 sys.path.insert(0, os.path.join(BASE_PATH, "_deps", "hydrobricks", "core", "src"))
 sys.path.insert(0, os.path.join(BASE_PATH, "_deps", "hydrobricks", "core", "bindings"))
 
+# -- Mock modules -------------------------------------------------------------
+
+from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+
+MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot', 'pandas', 'netCDF4',
+                'pytest', 'pyyaml',
+                ]
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
